4f991e1c42d1e7f56de03662f723c891b6fd640b
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-20 14:42:03 +0200

    Add files via upload

---
+++ PascalTriangle.java
@@ -0,0 +1,34 @@
+package pl.coderstrust.pascal;
+
+public class PascalTriangle
+{
+    public static void printPascalTriangle(int howManyRows)
+    {
+        int result;
+        for(int i = 0; i < howManyRows; i++)
+        {
+
+            for(int j = i; j <= howManyRows; j++)
+            {
+                System.out.print(String.format("%3s",""));
+            }
+
+            for(int k = 0; k <= i; k++)
+            {
+                result = factorial(i) / (factorial(k) * factorial(i-k));
+                System.out.print(String.format("%6s",result));
+            }
+            System.out.println();
+        }
+    }
+    private static int factorial(int getFactFrom)
+    {
+        int factorial = 1;
+        while(getFactFrom >= 1)
+        {
+            factorial *= getFactFrom;
+            getFactFrom--;
+        }
+        return factorial;
+    }
+}
