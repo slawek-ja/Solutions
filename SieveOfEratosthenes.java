9c5aba4907da48d098a64ef31a6fe10012c2a2ac
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-25 19:27:48 +0200

    Update SieveOfEratosthenes.java

--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -2,57 +2,46 @@ package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes
 {
-    public static int[] sieve(int maximumNumber)
-    {
-        if(maximumNumber <= 2) System.out.print(0);
+    public static int[] sieve(int maximumNumber) {
+        if (maximumNumber <= 2) System.out.print(0);
 
-        int tab[] = new int[maximumNumber-2];
-        int next_num = 2;
-        int find_index = 1;
+        int tab[] = new int[maximumNumber - 2];
+        int remainder = 2;
+        int findIndex = 1;
 
-        for (int i = 0; i < tab.length; i++)
-        {
-            tab[i] = i+2;
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = i + 2;
         }
 
-        while (next_num < maximumNumber)
-        {
-            for (int i = find_index+1; i < tab.length; i++)
-            {
-                if(tab[i] % next_num == 0)
-                {
+        while (remainder < maximumNumber) {
+            for (int i = findIndex + 1; i < tab.length; i++) {
+                if (tab[i] % remainder == 0) {
                     tab[i] = 0;
                 }
             }
-            next_num++;
-            for (int i = 0; i < tab.length; i++)
-            {
-                if (tab[i] >= next_num)
-                {
-                    next_num = tab[i];
-                    find_index = i;
+            remainder++;
+            for (int i = 0; i < tab.length; i++) {
+                if (tab[i] >= remainder) {
+                    remainder = tab[i];
+                    findIndex = i;
                     break;
                 }
             }
         }
-        int newTableLenght = 0;
-        for (int i = 0; i < tab.length; i++)
-        {
-            if (tab[i] != 0)
-            {
-                newTableLenght++;
+        int newTableLength = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] != 0) {
+                newTableLength++;
             }
         }
-        int resultTab[] = new int[newTableLenght];
-        find_index = 0;
-        for (int i = 0; i < tab.length; i++)
-        {
-            if(tab[i] != 0)
-            {
-                resultTab[find_index] = tab[i];
-                find_index++;
+        int result[] = new int[newTableLength];
+        findIndex = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] != 0) {
+                result[findIndex] = tab[i];
+                findIndex++;
             }
         }
-        return resultTab;
+        return result;
     }
 }
