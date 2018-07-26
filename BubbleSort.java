14620960a7aabdad1ae317713b318dcd5cf20f83
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-26 09:47:05 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,27 +1,22 @@
 package pl.coderstrust.sort;
 
-public class BubbleSort
-{
-    public static int[] sort(int[] array)
-    {
-        int [] tab = new int[array.length];
-        for (int i = 0; i < array.length; i++)
-        {
-            tab[i] = array[i];
-        }
-        int buff = 0;
-        for (int i = 0; i < tab.length; i++)
-        {
-            for (int j = 0; j < tab.length-1; j++)
-            {
-                if(tab[j] > tab[j+1])
-                {
-                    buff = tab[j];
-                    tab[j] = tab[j+1];
-                    tab[j+1] = buff;
+public class BubbleSort {
+    public static int[] sort(int[] array) {
+        int[] tab = java.util.Arrays.copyOf(array, array.length);
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    swap(tab, j, j + 1);
                 }
             }
         }
         return tab;
+    }
+
+    private static void swap(int tabA[], int a, int b) {
+        int buff = 0;
+        buff = tabA[a];
+        tabA[a] = tabA[b];
+        tabA[b] = buff;
     }
 }
