9e81aff362aacf7fcfc4320c30ae0f2ae04378be
slawek-ja <p.slawek90@gmail.com>
2018-07-26 17:12:39 +0200

    Fixed Structure

---
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -0,0 +1,15 @@
+package pl.coderstrust.foobar;
+
+public class FooBar
+{
+    public static void fooBar_display()
+    {
+        for (int i=0;i<=100;i++)
+        {
+            System.out.print(i + " ");
+            if(i % 3 == 0) System.out.print("Foo");
+            if(i % 5 == 0) System.out.print("Bar");
+            System.out.println(""); //next line after iteration
+        }
+    }
+}
