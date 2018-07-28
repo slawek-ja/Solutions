dc67e11b8dcb5188abb6f0d1cf4004921d3bd0d9
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-28 07:16:12 +0200

    Update SieveOfEratosthenes.java

--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -2,61 +2,62 @@ package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
     private static int multipleMarker = 0;
-    private static int index = 0; //needed to usage in methods
+    private static int markCurrentIndex = 0;
     public static int[] sieve(int maximumNumber) {
         if (maximumNumber <= 2){
             System.out.print(0);
         }
+        //fill table with potential numbers
         int tab[] = new int[maximumNumber];
         for (int i = 0; i < tab.length; i++) {
             tab[i] = i + 1;
         }
-        //erase first and last index
+        //erase first and last markCurrentIndex
         tab[0] = 0;
         tab[tab.length - 1] = 0;
         int lastPosIndex = 0;
         while (lastPosIndex < tab.length) {
-            //find next multiple to search
-            int multiple = findNextMultiple(tab,lastPosIndex);
-            lastPosIndex = index;
-            //jump to next index by += index and multiply result to prevent erase form array
-            index += multiple;
+            int multiple = findNextMultiple(tab, lastPosIndex);
+            lastPosIndex = markCurrentIndex;
+            markCurrentIndex += multiple;
             multiple += multiple;
-            //mark multiples and erase them by using last index
             markAsMultiple(tab,multiple,lastPosIndex);
-            index = lastPosIndex;
-        }
-        int howManyNum = 0;
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] != multipleMarker) {
-                howManyNum++;
-            }
+            markCurrentIndex = lastPosIndex;
         }
         //create new table and fill it with n != 0
-        int result[] = new int[howManyNum];
+        int result[] = new int[calculateMultiplies(tab)];
         collectResults(tab,result);
         return result;
     }
-    public static int findNextMultiple(int[] searchFrom ,int lastIndex){
+    private static int findNextMultiple(int[] searchFrom ,int lastIndex){
         int multiple = 0;
         for (int i = lastIndex; i < searchFrom.length; i++) {
             if (searchFrom[i] != 0) {
                 multiple = searchFrom[i];
-                index++;
+                markCurrentIndex++;
                 return multiple;
             }
-            index++;
+            markCurrentIndex++;
         }
         return multiple;
     }
-    public static void markAsMultiple(int[] numbers, int multiples, int lastPos){
-        for (int i = index; i <= numbers.length; i += lastPos, multiples += lastPos) {
+    private static void markAsMultiple(int[] numbers, int multiples, int lastPos){
+        for (int i = markCurrentIndex; i <= numbers.length; i += lastPos, multiples += lastPos) {
             if (numbers[i - 1] == multiples) {
                 numbers[i - 1] = 0;
             }
         }
     }
-    public static void collectResults(int[] pullFrom, int[] fill){
+    private static int calculateMultiplies(int[] numbers){
+        int howManyNum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != multipleMarker) {
+                howManyNum++;
+            }
+        }
+        return howManyNum;
+    }
+    private static void collectResults(int[] pullFrom, int[] fill){
         int index = 0;
         for (int i = 0; i < pullFrom.length; i++) {
             if (pullFrom[i] != multipleMarker) {
