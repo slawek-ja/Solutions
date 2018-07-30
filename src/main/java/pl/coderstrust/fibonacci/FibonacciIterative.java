b10e05232cee6cf68b40785e1b7e51bb1e9ae483
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-30 15:56:01 +0200

    Update FibonacciIterative.java

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -4,7 +4,7 @@ public class FibonacciIterative {
     public static long fibonacci(int fibonacciNumberInOrder){
         long firstNum = 0;
         long secondNum = 1;
-        for(int i=0;i<fibonacciNumberInOrder;i++)
+        for(int i = 0; i < fibonacciNumberInOrder; i++)
         {
             secondNum += firstNum;
             firstNum = secondNum - firstNum;
