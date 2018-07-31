b1a5ab98d0a803cc7f9d2aeef33e16b33e0d1eff
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-31 08:12:01 +0200

    Update SieveOfEratosthenes.java

--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -1,72 +1,41 @@
 package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
-
-    private static int multipleMarker = 0;
-    private static int markCurrentIndex = 0;
-
-    public static int[] sieve(int maximumNumber) {
-        if (maximumNumber <= 2){
-            System.out.print(0);
+    public static int[] sieve(int maximumNumber){
+        int[] table = fillTable(maximumNumber);
+        for (int i = 2; i*i < table.length; i++){
+            if(table[i] != 1){
+                for (int j = i*i; j < table.length; j+=i){
+                    table[j] = 0;
+                }
+            }
         }
-        //fill table with potential numbers
-        int tab[] = new int[maximumNumber];
-        for (int i = 0; i < tab.length; i++) {
-            tab[i] = i + 1;
-        }
-        //erase first and last markCurrentIndex
-        tab[0] = 0;
-        tab[tab.length - 1] = 0;
-        int lastPosIndex = 0;
-        while (lastPosIndex < tab.length) {
-            int multiple = findNextMultiple(tab, lastPosIndex);
-            lastPosIndex = markCurrentIndex;
-            markCurrentIndex += multiple;
-            multiple += multiple;
-            markAsMultiple(tab, multiple, lastPosIndex);
-            markCurrentIndex = lastPosIndex;
-        }
-        //create new table and fill it with n != 0
-        int[] result = collectResults(tab);
+        int[] result = returnResult(table);
         return result;
     }
-    private static int findNextMultiple(int[] numbers, int startIndex){
-        int multiple = 0;
-        for (int i = startIndex; i < numbers.length; i++) {
-            if (numbers[i] != 0) {
-                multiple = numbers[i];
-                markCurrentIndex++;
-                return multiple;
-            }
-            markCurrentIndex++;
+    private static int[] fillTable(int num){
+        int[] fillWithNumbers = new int[num];
+        for (int i = 0;i < fillWithNumbers.length; i++){
+            fillWithNumbers[i] = i;
         }
-        return multiple;
+        return fillWithNumbers;
     }
-    private static void markAsMultiple(int[] numbers, int multiple, int lastPos){
-        for (int i = markCurrentIndex; i <= numbers.length; i += lastPos, multiple += lastPos) {
-            if (numbers[i - 1] == multiple) {
-                numbers[i - 1] = multipleMarker;
+    private static int[] returnResult(int[] from){
+        from[1] = 0;
+        int countNum = 0;
+        for (int i : from) {
+            if(i != 0){
+                countNum++;
             }
         }
-    }
-    private static int countMultiples(int[] numbers){
-        int howManyNum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] != multipleMarker) {
-                howManyNum++;
-            }
-        }
-        return howManyNum;
-    }
-    private static int[] collectResults(int[] numbers){
+        int[] result = new int[countNum];
         int index = 0;
-        int[] fill = new int[countMultiples(numbers)];
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] != multipleMarker) {
-                fill[index] = numbers[i];
+        for (int i = 0; i < from.length; i++){
+            if(from[i] != 0){
+                result[index] = from[i];
                 index++;
             }
         }
-        return fill;
+            return result;
     }
 }
