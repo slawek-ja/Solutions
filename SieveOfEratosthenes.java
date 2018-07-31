f3e594f3bec18c943989a4eea2d08a91171c3100
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-07-31 22:46:58 +0200

    Update SieveOfEratosthenes.java

--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -1,38 +1,37 @@
 package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
-    public static int[] sieve(int maximumNumber){
-        if(maximumNumber <= 2){
+    public static int[] sieve(int maximumNumber) {
+        if(maximumNumber <= 2) {
             return new int[0];
         }
         int[] table = initNumbers(maximumNumber);
-        for (int i = 2; i*i < table.length; i++){
-                for (int j = i*i; j < table.length; j+=i){
+        for (int i = 2; i*i < table.length; i++) {
+                for (int j = i*i; j < table.length; j+=i) {
                     table[j] = 0;
                 }
         }
         int[] result = collectResults(table);
         return result;
     }
-    private static int[] initNumbers(int num){
+    private static int[] initNumbers(int num) {
         int[] result = new int[num];
-        for (int i = 0; i < result.length; i++){
+        for (int i = 0; i < result.length; i++) {
             result[i] = i;
         }
         return result;
     }
-    private static int[] collectResults(int[] numbers){
+    private static int[] collectResults(int[] numbers) {
         numbers[1] = 0;
         int numberOfPrimes = 0;
-        for (int i : numbers) {
-            if(i != 0){
+        for (int number : numbers) {
+            if(number != 0) {
                 numberOfPrimes++;
             }
         }
         int[] result = new int[numberOfPrimes];
-        int j = 0;
-        for (int i = 0; i < numbers.length; i++){
-            if(numbers[i] != 0){
+        for (int i = 0, j = 0; i < numbers.length; i++) {
+            if(numbers[i] != 0) {
                 result[j] = numbers[i];
                 j++;
             }
