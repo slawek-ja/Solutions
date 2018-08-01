0b36e3d424305b1f52a5d5b72dab7ae362713116
slawek-ja <p.slawek90@gmail.com>
2018-08-01 15:58:22 +0200

    Task #10 - FooBar - tests

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -1,15 +1,22 @@
 package pl.coderstrust.foobar;
 
-public class FooBar
-{
-    public static void printFooBar()
-    {
-        for (int i=0;i<=100;i++)
-        {
-            System.out.print(i + " ");
-            if(i % 3 == 0) System.out.print("Foo");
-            if(i % 5 == 0) System.out.print("Bar");
-            System.out.println(); //next line after iteration
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FooBar {
+    public static List<String> printFooBar(int size) {
+        if (size < 0){
+            return new ArrayList<String>(Collections.singletonList("Invalid Value"));
         }
+        List<String> result = new ArrayList<String>();
+        String storage;
+        for (int i=0;i<=size;i++) {
+            storage = i + " ";//System.out.print(i + " ");
+            if(i % 3 == 0) storage += "Foo";//System.out.print("Foo");
+            if(i % 5 == 0) storage += "Bar";//System.out.print("Bar");
+            result.add(storage);
+        }
+        return result;
     }
 }
