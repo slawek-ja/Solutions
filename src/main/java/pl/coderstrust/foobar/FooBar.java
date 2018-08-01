ca466723d82cf711372ac864f6fbef72617d9f54
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-08-01 18:51:58 +0200

    Update FooBar.java

--- src/main/java/pl/coderstrust/foobar/FooBar.java
+++ src/main/java/pl/coderstrust/foobar/FooBar.java
@@ -6,17 +6,21 @@ import java.util.List;
 
 public class FooBar {
     public static List<String> getFooBar(int size) {
-        if (size < 0){
+        if (size < 0) {
             return new ArrayList<String>(Collections.singletonList("Invalid Value"));
         }
         List<String> result = new ArrayList<String>();
         StringBuilder storage = new StringBuilder();
-        for (int i=0;i<=size;i++) {
+        for (int i=0; i <= size; i++) {
             storage.append(i + " ");
-            if(i % 3 == 0) storage.append("Foo");
-            if(i % 5 == 0) storage.append("Bar");
+            if(i % 3 == 0) {
+                storage.append("Foo");
+            }
+            if(i % 5 == 0) {
+                storage.append("Bar");
+            }
             result.add(storage.toString());
-            storage.delete(0,storage.length());
+            storage.delete(0, storage.length());
         }
         return result;
     }
