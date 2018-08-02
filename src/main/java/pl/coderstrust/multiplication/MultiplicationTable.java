e51aa461e5850587341b37846bd8d8d7e881bd14
slawek-ja <p.slawek90@gmail.com>
2018-08-02 11:52:07 +0200

    Task #11 - Multiplication Table - tests

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -1,28 +1,30 @@
 package pl.coderstrust.multiplication;
 
-public class MultiplicationTable
-{
-    public static void printMultiplicationTable(int size)
-    {
+public class MultiplicationTable {
+    public static int[][] getMultiplicationTable(int size) {
+        if (size < 1){
+            return new int[0][0];
+        }
         int row = 1;
         int column = 1;
+        int[][] result = new int[size][size];
+        //Printing given table
         System.out.print("  ");
-        for (int i = 1; i <= size; i++)
-        {
+        for (int i = 1; i <= size; i++) {
             System.out.print(String.format("%6s", i));
         }
         System.out.print("\n\n");
-        for (int i = 1; i <= size; i++)
-        {
+        for (int i = 1; i <= size; i++) {
             System.out.print(String.format("%2s", i));
-            for (int j = 1; j <= size; j++)
-            {
+            for (int j = 1; j <= size; j++) {
                 System.out.print(String.format("%6s", row * column));
+                result[row-1][column-1] = row*column;
                 column++;
             }
             row++;
             column = 1;
             System.out.print("\n\n");
         }
+        return result;
     }
 }
---
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -0,0 +1,59 @@
+package pl.coderstrust.multiplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiplicationTableTest {
+    @Test
+    public void testForNegative() {
+        //given
+        int size = -10;
+        int[][] expected = new int[0][0];
+
+        //when
+        int[][] array = MultiplicationTable.getMultiplicationTable(size);
+
+        //then
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testForZero() {
+        //given
+        int size = 0;
+        int[][] expected = new int[0][0];
+
+        //when
+        int[][] array = MultiplicationTable.getMultiplicationTable(size);
+
+        //then
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testForOne() {
+        //given
+        int size = 1;
+        int[][] expected = {{1}};
+
+        //when
+        int[][] array = MultiplicationTable.getMultiplicationTable(size);
+
+        //then
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testFirstFour() {
+        //given
+        int size = 4;
+        int[][] expected = {{1, 2, 3, 4},{2, 4, 6, 8},{3, 6, 9, 12}, {4, 8, 12, 16}};
+
+        //when
+        int[][] array = MultiplicationTable.getMultiplicationTable(size);
+
+        //then
+        assertArrayEquals(expected, array);
+    }
+}
