17bf7aa888c22cd0c4b8635be97880453b046d30
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-08-02 22:32:51 +0200

    Update MultiplicationTable.java

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -8,22 +8,13 @@ public class MultiplicationTable {
         int row = 1;
         int column = 1;
         int[][] result = new int[size][size];
-        //Printing given table
-        System.out.print("  ");
         for (int i = 1; i <= size; i++) {
-            System.out.print(String.format("%6s", i));
-        }
-        System.out.print("\n\n");
-        for (int i = 1; i <= size; i++) {
-            System.out.print(String.format("%2s", i));
             for (int j = 1; j <= size; j++) {
-                System.out.print(String.format("%6s", row * column));
                 result[row-1][column-1] = row*column;
                 column++;
             }
             row++;
             column = 1;
-            System.out.print("\n\n");
         }
         return result;
     }
