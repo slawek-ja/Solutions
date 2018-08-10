df6456742f2037cb9fdb45a06ff2ce61c574b805
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-08-10 14:28:32 +0200

    Update InsertionSort.java

--- src/main/java/pl/coderstrust/sort/InsertionSort.java
+++ src/main/java/pl/coderstrust/sort/InsertionSort.java
@@ -7,12 +7,12 @@ public class InsertionSort implements SortingMethod {
         int[] result = Arrays.copyOf(array, array.length);
         for (int i = 1; i < array.length; i++) {
             int key = result[i];
-            int j = i-1;
-            while (j>=0 && result[j] > key) {
-                result[j+1] = result[j];
-                j = j-1;
+            int pushNum = i-1;
+            while (pushNum >= 0 && result[pushNum] > key) {
+                result[pushNum+1] = result[pushNum];
+                pushNum = pushNum-1;
             }
-            result[j+1] = key;
+            result[pushNum+1] = key;
         }
         return result;
     }
