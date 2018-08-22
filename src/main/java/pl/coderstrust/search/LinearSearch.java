eba9e2605dee16614b2c47247317a0b4168124f4
slawek-ja <40492716+slawek-ja@users.noreply.github.com>
2018-08-22 21:03:42 +0200

    Update LinearSearch.java

--- src/main/java/pl/coderstrust/search/LinearSearch.java
+++ src/main/java/pl/coderstrust/search/LinearSearch.java
@@ -2,11 +2,11 @@ package pl.coderstrust.search;
 
 public class LinearSearch implements SearchingMethod {
     public int search(int[] array, int element) {
-        if (array.length == 0) {
-            throw new IllegalStateException("Array is empty. Cannot use searching method");
-        }
         if (array == null) {
-            throw new NullPointerException();
+            throw new IllegalArgumentException("Array cannot be null");
+        }
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Array is empty. Cannot use searching method");
         }
         for (int i = 0; i < array.length; i++) {
             if (array[i] == element) {
