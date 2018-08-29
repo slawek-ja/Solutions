47851e9160fa97f96b75cd4d5ff1667bdf4bb59d
slawek-ja <p.slawek90@gmail.com>
2018-08-30 01:50:33 +0200

    Update

--- src/test/java/pl/coderstrust/javaIO/IntegrationTests.java
+++ src/test/java/pl/coderstrust/javaIO/IntegrationTests.java
@@ -2,31 +2,49 @@ package pl.coderstrust.javaIO;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IntegrationTests {
 
+    private static final String inputFilePath = "src\\test\\java\\resources\\test_input.txt";
+    private static final String outputFilePath = "src\\test\\java\\resources\\test_output.txt";
+
+    private List<String> readFile(String fileInputPath) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileInputPath));
+        String currentLine;
+        List<String> result = new ArrayList<>();
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            result.add(currentLine);
+        }
+        return result;
+    }
+
+    @Before
+    public void removeOutputFileBeforeTest() {
+        File file = new File(outputFilePath);
+        file.delete();
+    }
+
     @Test
-    public void testForCorrectBehaviour() throws Exception {
+    public void testForCorrectBehaviour() throws IOException {
         //given
         NumbersProcessor numbersProcessor = new NumbersProcessor();
         FileProcessor fileProcessor = new FileProcessor();
         Processor processor = new Processor(numbersProcessor, fileProcessor);
-        String inputFilePath = "...\\solutions-7-slawek\\src\\test\\java\\resources\\inputTest";
-        String outputFilePath = "...\\solutions-7-slawek\\src\\test\\java\\resources\\outputTest";
-        List<String> givenInputResult = fileProcessor.readLinesFromFile(inputFilePath);
-        List<String> givenOutputResult = fileProcessor.readLinesFromFile(outputFilePath);
-        String bufferLine;
+        List<String> expected = readFile("src\\test\\java\\resources\\test_expected_output.txt");
 
         //when
         processor.process(inputFilePath, outputFilePath);
-        for (int i = 0; i < givenInputResult.size(); i++) {
-            bufferLine = numbersProcessor.processLine(givenInputResult.get(i));
-            givenInputResult.set(i, bufferLine);
-        }
+        List<String> processorOutputFile = readFile(outputFilePath);
 
         //then
-        assertEquals(givenOutputResult, givenInputResult);
+        assertEquals(expected, processorOutputFile);
     }
 }
